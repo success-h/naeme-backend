180628439325f24325878232c4b6c75e3ec9a137
Success Hycenth <successhycenth8@gmail.com>
2023-04-28 10:32:44 +0100

    update

--- naeme/settings.py
+++ naeme/settings.py
@@ -23,6 +23,7 @@ ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', "localhost"]
 CORS_ALLOWED_ORIGINS = [
     "https://naeme.vercel.app",
     "http://localhost:3000",
+    "https://co-lab-challange.vercel.app"
 ]
 
 
